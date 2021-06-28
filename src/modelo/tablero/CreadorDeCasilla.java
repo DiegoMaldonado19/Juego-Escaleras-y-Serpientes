56708a4a3a69419704b32aae047c5dbac3fe626d
Diego Maldonado <dj_maldonado19@hotmail.es>
2021-06-28 17:21:06 -0600

    Se agregaron nuevos cambios al frame de tablero

--- src/modelo/tablero/CreadorDeCasilla.java
+++ src/modelo/tablero/CreadorDeCasilla.java
@@ -6,6 +6,8 @@
 package modelo.tablero;
 
 import enums.TipoDeCasilla;
+import java.awt.Image;
+import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
@@ -25,10 +27,60 @@ public class CreadorDeCasilla {
         Casilla[][] tablero = new Casilla[cantidadCasillasX][cantidadCasillasY];
         for (int i = 0; i < cantidadCasillasX; i++) {
             for (int j = 0; j < cantidadCasillasY; j++) {
-                SuperficieJLabel jlabel = new SuperficieJLabel(i+1, j+1,"Casilla ("+(i+1)+","+(j+1)+")");
-              
-                tableroAux = new Casilla(TipoDeCasilla.VACIA, jlabel,false);
-                tablero[i][j] = tableroAux;
+                int random = (int)(Math.random()*100);
+                
+                if(random>=50 && random<=60){
+                    SuperficieJLabel jlabel = new SuperficieJLabel(i+1, j+1,String.valueOf(TipoDeCasilla.RETROCESO));
+                    Image img= new ImageIcon("imagenes/cuadrado.png").getImage();
+                    ImageIcon img2=new ImageIcon(img.getScaledInstance(20, 40, Image.SCALE_SMOOTH));
+                    jlabel.setIcon(img2);
+
+                    tableroAux = new Casilla(TipoDeCasilla.VACIA, jlabel,false);
+                    tablero[i][j] = tableroAux;
+                    
+                }
+                else if(random>60 && random<=70){
+                    SuperficieJLabel jlabel = new SuperficieJLabel(i+1, j+1,String.valueOf(TipoDeCasilla.ADELANTO));
+                    Image img= new ImageIcon("imagenes/cuadrado.png").getImage();
+                    ImageIcon img2=new ImageIcon(img.getScaledInstance(20, 40, Image.SCALE_SMOOTH));
+                    jlabel.setIcon(img2);
+                    tableroAux = new Casilla(TipoDeCasilla.VACIA, jlabel,false);
+                    tablero[i][j] = tableroAux;
+                     
+                }
+                else if(random>70 && random<=80){
+                    SuperficieJLabel jlabel = new SuperficieJLabel(i+1, j+1,String.valueOf(TipoDeCasilla.ESCALERA));
+                    Image img= new ImageIcon("imagenes/cuadrado.png").getImage();
+                    ImageIcon img2=new ImageIcon(img.getScaledInstance(20, 40, Image.SCALE_SMOOTH));
+                    jlabel.setIcon(img2);
+                    tableroAux = new Casilla(TipoDeCasilla.VACIA, jlabel,false);
+                    tablero[i][j] = tableroAux;
+                }
+                else if(random>80 && random<=90){
+                    SuperficieJLabel jlabel = new SuperficieJLabel(i+1, j+1,String.valueOf(TipoDeCasilla.SERPIENTE));
+                    Image img= new ImageIcon("imagenes/cuadrado.png").getImage();
+                    ImageIcon img2=new ImageIcon(img.getScaledInstance(20, 40, Image.SCALE_SMOOTH));
+                    jlabel.setIcon(img2);
+                    tableroAux = new Casilla(TipoDeCasilla.VACIA, jlabel,false);
+                     tablero[i][j] = tableroAux;
+                }
+                else if(random>=0 && random<50){
+                    SuperficieJLabel jlabel = new SuperficieJLabel(i+1, j+1,String.valueOf(TipoDeCasilla.VACIA));
+                    Image img= new ImageIcon("imagenes/cuadrado.png").getImage();
+                    ImageIcon img2=new ImageIcon(img.getScaledInstance(20, 40, Image.SCALE_SMOOTH));
+                    jlabel.setIcon(img2);
+                    tableroAux = new Casilla(TipoDeCasilla.VACIA, jlabel,false);
+                    tablero[i][j] = tableroAux;
+                }
+                else if(random>90 && random<=100){
+                    SuperficieJLabel jlabel = new SuperficieJLabel(i+1, j+1,String.valueOf(TipoDeCasilla.PIERDE_TURNO)+" ");
+                    Image img= new ImageIcon("imagenes/cuadrado.png").getImage();
+                    ImageIcon img2=new ImageIcon(img.getScaledInstance(20, 40, Image.SCALE_SMOOTH));
+                    jlabel.setIcon(img2);
+                    tableroAux = new Casilla(TipoDeCasilla.VACIA, jlabel,false);
+                    tablero[i][j] = tableroAux;
+                }
+               
             }
             
         }
